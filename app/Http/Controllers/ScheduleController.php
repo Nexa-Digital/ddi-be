a4a4ee8cf4fff5072a5a49e6b58e3e8d1be82c38
Rahmat Riyadi Syam <rahmatriyadisyam@MacBook-Pro-Macbook.local>
2025-04-06 14:55:07 +0800

    feat: ui

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\ScheduleRepository;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ScheduleController extends Controller
 {
@@ -31,8 +35,67 @@ class ScheduleController extends Controller
     }
 
     public function store(Request $request){
-        $this->repository->create($request->all());
-        return response()->json(null, 201);
+
+        DB::beginTransaction();
+        
+        try {
+            $start = Carbon::parse($request->start_date);
+            $end = Carbon::parse($request->end_date);
+
+            $dayMap = [
+                'senin' => 'monday',
+                'selasa' => 'tuesday',
+                'rabu' => 'wednesday',
+                'kamis' => 'thursday',
+                'jumat' => 'friday',
+                'sabtu' => 'saturday',
+                'minggu' => 'sunday',
+            ];
+            //code...
+            $period = CarbonPeriod::create($start, $end);
+            $insertData = [];
+
+            $requestedDay = $dayMap[$request->day];
+        
+            foreach ($period as $date) {
+                $dayName = strtolower($date->format('l')); // misal: monday, tuesday
+                if ($requestedDay === $dayName) {
+                    // Cek apakah data sudah ada
+                    $exists = DB::table('schedules') // Ganti 'schedules' dengan nama tabel kamu
+                        ->where('date', $date->format('Y-m-d'))
+                        ->where('user_id', $request->user_id)
+                        ->where('role_id', $request->role_id)
+                        ->where('class_room_id', $request->class_room_id)
+                        ->where('start', $request->start)
+                        ->exists();
+
+                    if (!$exists) {
+                        $insertData[] = [
+                            'date' => $date->format('Y-m-d'),
+                            'start' => $request->start,
+                            'finish' => $request->finish,
+                            'amount' => $request->amount,
+                            'role_id' => $request->role_id,
+                            'class_room_id' => $request->class_room_id,
+                            'user_id' => $request->user_id,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ];
+                    }
+                }
+            }
+
+            $this->repository->getModel()->insert($insertData);
+            // return response()->json($insertData);
+            // $this->repository->create($request->all());
+            DB::commit();
+            return response()->json(null, 201);
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+
     }
 
     public function update($id, Request $request){
